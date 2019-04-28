3bc79222d27ea7004f69f21654effb7a7614a3bc
A1phaZer0 <ini.universe@gmail.com>
2019-04-28 10:02:28 +0800

    update

---
+++ list_dir-x86.s
@@ -0,0 +1,64 @@
+.intel_syntax noprefix
+.global _start
+
+_start:
+	jmp REL_JMP
+REL_CALL:
+	pop ebx
+        xor edx, edx
+	mov byte ptr [ebx + 1], dl # NULL-terminated dir name
+	push 04
+	pop edx
+	xor ecx, ecx
+	push 5
+	pop eax  
+	int 0x80      # syscall open
+
+	mov ecx, ebx # dirent
+	mov esi, ecx
+	mov ebx, eax # fd
+	xor edx, edx
+	mov dx, 0x1010
+	push 141
+	pop eax
+	int 0x80      # syscall getdents
+
+PRINT_LOOP:
+	xor eax, eax
+	mov al, 0xaa
+	xor al, 0xa0 # get '\n'
+
+	xor edx, edx
+	mov dx, word ptr [esi + 8] # len of dirent
+	add edx, esi
+	mov byte ptr [edx - 1], al  # overwrite d_type append '\n'
+
+	xor edx, edx
+	mov dx, word ptr [esi + 8] 
+	sub edx, 10  # len of string
+	lea ecx, [esi + 10]
+	push 1
+	pop ebx      # fd
+	push 4
+	pop eax
+	int 0x80      # syscall write
+	
+	xor edx, edx
+	mov dx, word ptr [esi + 8]
+
+	mov ecx, 0x7fffffff  # no next entry
+	cmp dword ptr [esi + 4], ecx # d_off of linux_dirent
+	lea esi, [esi + edx]         # next dirent
+
+	jnz PRINT_LOOP
+EXIT:
+	xor ebx, ebx # error code 
+	push 1
+	pop eax
+	int 0x80      # exit normally
+
+REL_JMP:
+	call REL_CALL
+	.ascii "." # put linux_dirent here too.
+
+	
---
+++ list_dir-x86_64.s
@@ -0,0 +1,65 @@
+.intel_syntax noprefix
+.global _start
+
+_start:
+	jmp REL_JMP
+REL_CALL:
+	pop rdi
+        xor rdx, rdx
+	mov byte ptr [rdi + 1], dl # NULL-terminated dir name
+	push 04
+	pop rdx
+	xor rsi, rsi
+	push 0x2
+	pop rax  
+	syscall      # syscall open
+
+	mov rsi, rdi # dirent
+	mov rdi, rax # fd
+	xor rdx, rdx
+	mov dx, 0x1010
+	push 78
+	pop rax
+	syscall      # syscall getdents
+
+PRINT_LOOP:
+	xor rax, rax
+	mov al, 0xaa
+	xor al, 0xa0 # get '\n'
+
+	xor rdx, rdx
+	mov dx, word ptr [rsi + 16] # len of dirent
+	add rdx, rsi
+	mov byte ptr [rdx - 1], al  # overwrite d_type append '\n'
+
+	xor rdx, rdx
+	mov dx, word ptr [rsi + 16] 
+	sub rdx, 18  # len of string
+	push rsi     # save rsi
+	lea rsi, [rsi + 18]
+	push 1
+	pop rdi      # fd
+	push 1
+	pop rax
+	syscall      # syscall write
+	
+	pop rsi      # recover rsi
+	xor rdx, rdx
+	mov dx, word ptr [rsi + 16]
+
+	mov rcx, 0x7fffffffffffffff  # no next entry
+	cmp qword ptr [rsi + 8], rcx # d_off of linux_dirent
+	lea rsi, [rsi + rdx]         # next dirent
+
+	jnz PRINT_LOOP
+EXIT:
+	xor rdi, rdi # error code 
+	push 60
+	pop rax
+	syscall      # exit normally
+
+REL_JMP:
+	call REL_CALL
+	.ascii "." # put linux_dirent here too.
+
+	
